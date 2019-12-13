ec42dfa59c00d92fa71f400a0735e8c3c00734eb
haloscot <hannahlorrainescott@gmail.com>
2019-12-13 02:01:49 -0500

    finally figured out local storage maybe?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,33 @@
-$(document).ready(function() {
-
-  //push date to bottom of jumbotron
+  
   var now = moment().format('DD MMM YYYY');
   $('#currentDay').text(now);
 
-  //get the hour of day to set colours for hour element.
   var hourEl = moment().get('hour');
 
-  var test = $(".hour");
-
-  // change colour of time span based on time of day.
-  $.each(test, function() {
-      if ($( this ).attr('value') > hourEl) {
-          test.addClass( "future" );
-          console.log(test);
+  $(".hour").each(function() {
+      var timeIndex = parseInt($(this).attr('value'));
+      if (timeIndex < hourEl){
+          $(this).addClass( "past" );
       }
-      else if ($( this ).attr('value') === hourEl) {
-          test.addClass( "present" );
+      if (timeIndex === hourEl) {
+          $(this).addClass( "present" );
       }
-      else {
-          test.addClass( "past" );
+      if (timeIndex > hourEl) {
+          $(this).addClass( "future" );
       }
   });
 
-
-  //Event listener for all buttons
-  $('#saveBtn').click(function(event) {
-      var valueAttr = ("data-hour").val();
-      var notes = JSON.parse(window.localStorage.getItem("#description"));
-      console.log('clicked', $(this.text()));
-      alert("Save button clicked!");
+  $(".saveBtn").on("click" , function(event) {
+    var num = event.currentTarget.id;
+    localStorage.setItem("schedInput" + num , $("#block-" + num).text());
   });
 
-  $("#saveBtn").click(function() {
-      var value = $("#saveBtn").index(this)
-      console.log(value);
-  });
-});
 
-// additional functions:
 
-// Copy text from text area to clipboard
+  init();
 
-// function copyToClipboard() {
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.select();
-//   document.execCommand("copy");
-//   alert(
-//     "Your password " + passwordText.value + " was copied to your clipboard."
-//   );
-// }
-
-// local storage set/get
-
-// function printHighscores() {
-//   // either get scores from localstorage or set to empty array
-//   var notes = JSON.parse(window.localStorage.getItem("#description")) || [];
-
-//   // sort highscores by score property in descending order
-//   notes.sort(function(a, b) {
-//     return b.score - a.score;
-//   });
-// }
+  function init() {
+    for (var i = 9; i < 18; i++) {
+       $("#block-" + i).text(localStorage.getItem("schedInput" + i));
+      }
+    };
