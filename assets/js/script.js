d025c6b931f56709896639eb6d7f737cc316e3cd
haloscot <hannahlorrainescott@gmail.com>
2019-12-12 23:59:04 -0500

    updating js and expanded html to include calendar times

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,33 @@ $( document ).ready(function() {
 
 var now = moment().get('hour');
     console.log(date.hour);
+
+    var content = $('.content')
+    console.log(content);
+
+    content.text("testing");
+
+
+    $(document).on('click', '.content', function(event)
+{
+	event.preventDefault();
+
+	if($(this).attr('edit_type') == 'button')
+	{
+		return false;
+	}
+
+	//make div editable
+	$(this).closest('div').attr('contenteditable', 'true');
+	//add bg css
+	$(this).addClass('bg-warning');
+
+	$(this).focus();
+});
+
+$(document).on('blur', '.content', function(event) {
+    $(this).closest('div').attr('contenteditable', 'false');
+    $(this).removeClass('bg-warning');
+    // $(this).unfocus();
+
+});
